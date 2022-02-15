cb601dd1280387733de2ad5df4e4a036f77ab0a7
kiruto398 <52631675+kiruto398@users.noreply.github.com>
2022-02-15 11:59:02 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,28 +1,104 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <title>popup</title>
-  <style>
-    body{
-      width: 300px;
+"use strict";
+
+const dom_remaining_time = document.getElementById('remaining_time');
+const dom_score = document.getElementById('score');
+const dom_time = document.getElementById('time');
+const dom_realtime_rank = document.getElementById('realtime_rank');
+const dom_realtime_perf = document.getElementById('realtime_perf');
+const dom_final_rank = document.getElementById('final_rank');
+const dom_final_perf = document.getElementById('final_perf');
+
+timer();
+setInterval(timer, 1000);
+
+let cnter = true;
+function timer(){
+  chrome.runtime.sendMessage({mode: "popup"});
+  const res = chrome.extension.getBackgroundPage().getResults();
+
+  if(res.my.score === null){
+    dom_remaining_time.textContent = '-';
+    dom_score.textContent = '';
+    dom_time.textContent = '';
+    dom_realtime_rank.textContent = '';
+    dom_realtime_perf.textContent = '';
+    dom_final_rank.textContent = '';
+    dom_final_perf.textContent = '';
+
+  }else if(res.my.elapse >= 0){
+    if(res.my.elapse > 0){
+      let now = new Date();
+      now.setMilliseconds(0);
+      let remain = Number.parseInt((res.my.endTime - now)/1000);
+      const s = ('0' + remain%60).slice(-2);
+      remain = Number.parseInt(remain/60);
+      const m = ('0' + remain%60).slice(-2);
+      remain = Number.parseInt(remain/60);
+      const h = ('0' + parseInt(remain%24)).slice(-2);
+      const d = Number.parseInt(remain/24);
+
+      let day_str = '';
+      if(d >= 1){
+        day_str = '' + d + '日 ';
+      }
+
+      dom_remaining_time.textContent = day_str + h + ':' + m + ':' + s;
+    }else{
+      dom_remaining_time.textContent = '-';
     }
-  </style>
-</head>
-<body>
-  <div style='line-height:50%'>
-  <p style='font-size:x-large'> 残り時間　<span id='remaining_time'></span></p>
-  <p style='font-size:large'> SCORE　:　<span id='score'></span></p>
-  <p style='font-size:large'> TIME　:　<span id='time'></span></p>
-  <p>　</p>
-  <p style='font-size:large'> 現在の順位　　　<span id='realtime_rank'></span></p>
-  <p style='font-size:large'> パフォーマンス　<span id='realtime_perf'></span></p>
-  <p>　</p>
-  <p style='font-size:large'> 終了時の順位　　<span id='final_rank'></span></p>
-  <p style='font-size:large'> パフォーマンス　<span id='final_perf'></span></p>
-  </div>
 
+    dom_score.textContent = add_commas(res.my.score);
+    dom_time.textContent = Number.parseInt(res.my.validElapse/60) + ':' + ('0' + res.my.validElapse%60).slice(-2);
+    dom_realtime_rank.textContent = add_commas(res.opt.displayRank ? res.my.realtimeRatedRank : res.my.realtimeRank) + ' / ' + add_commas(res.opt.displayRank ? res.contest.ratedEntryCount : res.contest.entryCount);
+    dom_realtime_perf.textContent = add_commas(res.my.realtimePerf);
+    dom_realtime_perf.style = 'color : ' + get_color(res.my.realtimePerf) + ';';
+    dom_final_rank.textContent = add_commas(res.opt.displayRank ? res.my.finalRatedRank : res.my.finalRank) + ' / ' + add_commas(res.opt.displayRank ? res.contest.ratedEntryCount : res.contest.entryCount);
+    dom_final_perf.textContent = add_commas(res.my.finalPerf);
+    dom_final_perf.style = 'color : ' + get_color(res.my.finalPerf) + ';';
 
-  <script src='popup.js'></script>
-</body>
-</html>
+  }else{
+    dom_remaining_time.textContent = '-';
+    dom_score.textContent = add_commas(res.my.score);
+    dom_time.textContent = Number.parseInt(res.my.validElapse/60) + ':' + res.my.validElapse%60;
+    dom_realtime_rank.textContent = '';
+    dom_realtime_perf.textContent = '';
+    dom_final_rank.textContent = add_commas(res.opt.displayRank ? res.my.finalRatedRank : res.my.finalRank) + ' / ' + add_commas(res.opt.displayRank ? res.contest.ratedEntryCount : res.contest.entryCount);
+    dom_final_perf.textContent = add_commas(res.my.finalPerf);
+    dom_final_perf.style = 'color : ' + get_color(res.my.finalPerf) + ';';
+  }
+}
+
+function add_commas(num){
+  let ret = '';
+  if(Number.isInteger(num)){
+    ret = num.toLocaleString();
+  }else{
+  }
+
+  return ret;
+}
+
+function get_color(rate){
+  let color = null;
+  if(rate != null){
+    if(rate < 400){
+      color = 'black';
+    }else if(rate < 800){
+      color = 'brown';
+    }else if(rate < 1200){
+      color = 'green';
+    }else if(rate < 1600){
+      color = 'aqua';
+    }else if(rate < 2000){
+      color = 'blue';
+    }else if(rate < 2400){
+      color = 'gold';
+    }else if(rate < 2800){
+      color = 'orange';
+    }else{
+      color = 'red';
+    }
+  }
+
+  return color;
+}
